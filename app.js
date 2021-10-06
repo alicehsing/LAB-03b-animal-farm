85228448b0e80f9e785f68685ddcb161b61c88d2
Alice Hsing <alicehsing@gmail.com>
2021-10-06 16:24:32 -0700

    added sound to image

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 // import functions
-
 // reference needed DOM elements
+const catImage = document.getElementById('cat-image');
+const catSound = document.getElementById('cat-sound');
 
+//"state" - things that change over time
+let catClicks = 0;
 
 // set event listeners 
-    // get user input(s)
-    // do any needed work with the value(s)
-    // update DOM to reflect new value(s)
+catImage.addEventListener('click', () => {
+    catSound.play();
+    catClicks = catClicks + 1;
+    console.log('cat clicks is now', catClicks);
+
+});
+
+const dogImage = document.getElementById('dog-image');
+const dogSound = document.getElementById('dog-sound');
+
+let dogClicks = 0;
+
+dogImage.addEventListener('click', () => {
+    dogSound.play();
+    dogClicks = dogClicks + 1;
+    console.log('dog clicks is now', dogClicks);
+
+});
+
+const horseImage = document.getElementById('horse-image');
+const horseSound = document.getElementById('horse-sound');
+
+horseImage.addEventListener('click', () => {
+    horseSound.play();
+});
